f5473eebb665a6af035eee03955d402bdd0f891e
lucaspetti <luc_petti@hotmail.com>
2023-11-13 16:41:34 -0300

    Add user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'columns' do
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:first_name).of_type(:string) }
+    it { is_expected.to have_db_column(:last_name).of_type(:string) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+  end
+end
